6fef4ef39ec28fbcd1e1f7668cf8aff547dbbb01
Alyaksey Lanewski <lanevskialex1996@gmail.com>
2025-01-29 16:21:49 +0300

    Create project

---
+++ samplesite/books/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length = 50)
+    author = models.CharField(max_length = 50)
+    description = models.TextField(null = True, blank = True)
+    price = models.FloatField(null = True, blank = True)
+    published = models.DateField(auto_now_add = True, db_index = True)
+
---
+++ samplesite/books/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+from .models import Book
+
+def index(request):
+    books = Book.objects.all()
+    return render(request, 'books/index.html', {'books': books})
