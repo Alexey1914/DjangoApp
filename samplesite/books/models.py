f4bd1191eac644eacf89816ba8d451fd0614e90e
Alyaksey Lanewski <lanevskialex1996@gmail.com>
2025-01-30 19:51:41 +0300

    Create user in /admin

--- samplesite/books/models.py
+++ samplesite/books/models.py
@@ -7,3 +7,10 @@ class Book(models.Model):
     price = models.FloatField(null = True, blank = True)
     published = models.DateField(auto_now_add = True, db_index = True)
 
+    def __str__(self):
+        return self.title + " | " + self.author
+
+    class Meta:
+        verbose_name_plural = 'Книги'
+        verbose_name = 'Книга'
+        ordering = [ 'id' ]
